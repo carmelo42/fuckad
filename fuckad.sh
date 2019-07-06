8f1191c0200d44eb322913d05aadb7d389c7e4ac
Carmelo <carmelo@ingrao.fr>
2019-07-06 17:54:47 +0200

    correction

--- fuckad.sh
+++ fuckad.sh
@@ -6,7 +6,8 @@ gunzip hosts.gz
 mv hosts hosts_ybad
 cat hosts_seb >> hosts_ybad 
 sort hosts_ybad | uniq >> hosts
-cat hosts >> /etc/hosts
+sed -i '1s/^/127.0.0.1 localhost /' hosts
+cp hosts /etc/hosts
 rm hosts
 rm hosts_seb
 rm hosts_ybad
